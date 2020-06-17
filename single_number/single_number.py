ab990984dc778cf421f638f3b0a916edc7aed976
evan <aspaasevan@gmail.com>
2020-06-17 15:27:40 -0700

    single number test passes

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,15 +3,14 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-    # Given an arr - assume there will always a single int 
-    # 
-    #   
-    pass
-
+    result = 0
+    for i in arr:
+        result ^= i
+    return result
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    # arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    pass
 
-    print(f"The odd-number-out is {single_number(arr)}")
+    # print(f"The odd-number-out is {single_number(arr)}")
